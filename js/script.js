e958996fbf1c41e7afc3338c511abcc5c569fc21
belov11 <86915472+belov11@users.noreply.github.com>
2023-10-10 12:11:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function clock() {
     let mounth = date.getMonth();
     const weekDay = {
         1: "Monday",
-        2: "Tuesdays",
+        2: "Tuesday",
         3: "Wednesday",
         4: "Thursday",
         5: "Friday",
